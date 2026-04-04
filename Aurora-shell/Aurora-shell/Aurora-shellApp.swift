53a68e27af71cead1f675052d4d62b30f155f996
YashB-byte <yash.behera@icloud.com>
2026-04-04 08:28:06 -0700

    update aurora-shell

---
+++ Aurora-shell/Aurora-shell/Aurora-shellApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct Aurora_shellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.hiddenTitleBar)
+    }
+}
+
